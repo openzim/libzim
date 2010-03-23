2c185bfcfd234755556af6c772353ac46879e698
tntnet <tntnet@localhost>
2010-03-23 23:19:03 +0000

    add files for symbian support

---
+++ symbian/config.h
@@ -0,0 +1,87 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.in by autoheader.  */
+
+/* set cluster cache size to number of cached chunks */
+#define CLUSTER_CACHE_SIZE 1
+
+/* set dirent cache size to number of cached chunks */
+#define DIRENT_CACHE_SIZE 512
+
+/* defined if bzip2 compression is enabled */
+  #define ENABLE_BZIP2 1
+
+/* defined if lzma compression is enabled */
+ #define ENABLE_LZMA 1
+
+/* defined if zlib compression is enabled */
+/* #undef ENABLE_ZLIB */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `stat64' function. */
+/* #undef HAVE_STAT64 */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* set lzma uncompress memory size to number of MB */
+#define LZMA_MEMORY_SIZE 128
+
+/* Name of package */
+#define PACKAGE "zimlib"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "Tommi Maekitalo <tommi@tntnet.org>"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "zimlib"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "zimlib 0.92.20091227-3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "zimlib"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.92.20091227-3"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.92.20091227-3"
+
+/* defined if cxxtools is used */
+/* #undef WITH_CXXTOOLS */
