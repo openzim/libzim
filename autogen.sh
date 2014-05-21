c053736b2806ead4df49d088f8730a0e81d194f6
kelson42 <kelson@kiwix.org>
2014-05-21 11:12:57 +0200

    + On OSX, 'libtoolize' is called 'glibtoolize'

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 # Regenerate the files autoconf / automake
 
-libtoolize --force --automake
+if [ "`(uname -s) 2>/dev/null`" = "Darwin" ]
+then
+    LIBTOOLIZE=glibtoolize
+else
+    LIBTOOLIZE=libtoolize
+fi
+$LIBTOOLIZE --force --automake
 
 rm -f config.cache config.log
 aclocal -I m4
