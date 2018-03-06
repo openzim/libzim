4bdbb6d66324d23b91687d788c88e3a758db9811
Matthieu Gautier <mgautier@kymeria.fr>
2018-03-06 15:42:20 +0100

    Fix "stringification" of the checksum.
    Bit operations work on unsigned char, not signed ones.

--- src/fileimpl.cpp
+++ src/fileimpl.cpp
@@ -500,8 +500,9 @@ namespace zim
     char* p = hexdigest;
     for (int i = 0; i < 16; ++i)
     {
-      *p++ = hex[chksum->at(offset_t(i)) >> 4];
-      *p++ = hex[chksum->at(offset_t(i)) & 0xf];
+      uint8_t v = chksum->at(offset_t(i));
+      *p++ = hex[v >> 4];
+      *p++ = hex[v & 0xf];
     }
     log_debug("chksum=" << hexdigest);
     return hexdigest;
