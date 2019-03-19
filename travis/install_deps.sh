99b3a66ac1ba8894bfdf787ce9d45886c3415e10
Matthieu Gautier <mgautier@kymeria.fr>
2019-03-19 14:34:04 +0100

    Force use of meson 0.49.2

--- travis/install_deps.sh
+++ travis/install_deps.sh
@@ -11,14 +11,14 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]
 then
   brew update
   brew upgrade python3
-  pip3 install meson pytest
+  pip3 install meson==0.49.2 pytest
 
   wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip
   unzip ninja-mac.zip ninja
 else
   wget https://bootstrap.pypa.io/get-pip.py
   python3.5 get-pip.py --user
-  python3.5 -m pip install --user meson
+  python3.5 -m pip install --user meson==0.49.2
 
   wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
   unzip ninja-linux.zip ninja
