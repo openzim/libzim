927160f39ba8d51985b6a7358b982e28afe140b3
Matthieu Gautier <mgautier@kymeria.fr>
2019-09-10 11:41:53 +0200

    [CI] Install brew things with travis howebrew addon instead of script.

--- travis/install_deps.sh
+++ travis/install_deps.sh
@@ -9,9 +9,6 @@ ARCHIVE_NAME=deps_${TRAVIS_OS_NAME}_${PLATFORM}_${REPO_NAME}.tar.xz
 cd $HOME
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]
 then
-  brew update
-  brew install gcovr
-#  brew upgrade python3
   pip3 install meson==0.49.2 pytest
 
   wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip
