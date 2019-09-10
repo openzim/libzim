c36d36dc5d66b1a595266fd67dba05e221c58962
Matthieu Gautier <mgautier@kymeria.fr>
2019-09-10 11:17:07 +0200

    Install pytest in the CI.

--- travis/install_deps.sh
+++ travis/install_deps.sh
@@ -19,7 +19,7 @@ then
 else
   wget https://bootstrap.pypa.io/get-pip.py
   python3.5 get-pip.py --user
-  python3.5 -m pip install --user meson==0.49.2
+  python3.5 -m pip install --user meson==0.49.2 pytest
 
   wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
   unzip ninja-linux.zip ninja
