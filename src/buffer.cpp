02924b436ae36f81d1bb3359df2a80c1b8cceebb
Steve Wills <steve@mouf.net>
2020-07-17 19:51:17 -0400

    Fix build on FreeBSD

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -48,6 +48,8 @@ MMapBuffer::MMapBuffer(int fd, offset_t offset, zsize_t size):
   _offset = offset-pa_offset;
 #if defined(__APPLE__) || defined(__OpenBSD__)
   #define MAP_FLAGS MAP_PRIVATE
+#elif defined(__FreeBSD__)
+  #define MAP_FLAGS MAP_PRIVATE|MAP_PREFAULT_READ
 #else
   #define MAP_FLAGS MAP_PRIVATE|MAP_POPULATE
 #endif
--- src/fs_unix.cpp
+++ src/fs_unix.cpp
@@ -34,7 +34,7 @@ namespace unix {
 
 zsize_t FD::readAt(char* dest, zsize_t size, offset_t offset) const
 {
-#if defined(__APPLE__) || defined(__OpenBSD__)
+#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__)
 # define PREAD pread
 #else
 # define PREAD pread64
