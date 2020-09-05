1ac51bdb0a3119fe47264e53786eea4be8677119
Veloman Yunkan <veloman.yunkan@gmail.com>
2020-09-05 16:31:44 +0400

    DecodedDataStreamTest.largeCompressedData
    Added a unit-test covering the scenario where the loop body in
    `DecodedDataStream::readImpl()` has to be executed more than once.

--- test/decodeddatastream.cpp
+++ test/decodeddatastream.cpp
@@ -45,6 +45,38 @@ std::string operator*(const std::string& s, unsigned N)
   return result;
 }
 
+std::string
+mutate(std::string str, size_t i)
+{
+  const size_t n = str.size();
+  if ( i >= n )
+    str = mutate(str, i/n);
+
+  str[i%n] = '!';
+
+  return str;
+}
+
+std::string
+largeNotEasilyCompressibleString()
+{
+  const int N = 5000;
+  const std::string s("DecodedDataStream should work correctly");
+  std::string text;
+  for (size_t i=0; i<N; i++)
+    text += mutate(s, i);
+
+  return text;
+}
+
+std::unique_ptr<zim::IDataStream>
+makeBufDataStream(const std::string* s)
+{
+  const char* const data = s->data();
+  const size_t size = s->size();
+  return std::unique_ptr<zim::IDataStream>(new zim::BufDataStream(data, size));
+}
+
 std::string toString(const zim::IDataStream::Blob& blob)
 {
   return std::string(blob.data(), blob.size());
@@ -66,14 +98,15 @@ using CompressionTypes = ::testing::Types<
 
 TYPED_TEST_CASE(DecodedDataStreamTest, CompressionTypes);
 
-TYPED_TEST(DecodedDataStreamTest, justCompressedData) {
+TYPED_TEST(DecodedDataStreamTest, smallCompressedData)
+{
   typedef typename TestFixture::CompressionInfo CompressionInfo;
 
   const int N = 10;
   const std::string s("DecodedDataStream should work correctly");
   const std::string compData = compress<CompressionInfo>(s*N);
 
-  std::unique_ptr<zim::IDataStream> bds(new zim::BufDataStream(compData.data(), compData.size()));
+  std::unique_ptr<zim::IDataStream> bds(makeBufDataStream(&compData));
   zim::DecodedDataStream<CompressionInfo> dds(std::move(bds), compData.size());
   for (int i=0; i<N; i++)
   {
@@ -81,7 +114,21 @@ TYPED_TEST(DecodedDataStreamTest, justCompressedData) {
   }
 }
 
-TYPED_TEST(DecodedDataStreamTest, compressedDataFollowedByGarbage) {
+TYPED_TEST(DecodedDataStreamTest, largeCompressedData)
+{
+  typedef typename TestFixture::CompressionInfo CompressionInfo;
+
+  const std::string text = largeNotEasilyCompressibleString();
+  const std::string compData = compress<CompressionInfo>(text);
+  ASSERT_GT(compData.size(), 2 * 1024); // 1024 is DecodedDataStream::CHUNK_SIZE
+
+  std::unique_ptr<zim::IDataStream> bds(makeBufDataStream(&compData));
+  zim::DecodedDataStream<CompressionInfo> dds(std::move(bds), compData.size());
+  ASSERT_EQ(text, toString(dds.readBlob(text.size())));
+}
+
+TYPED_TEST(DecodedDataStreamTest, compressedDataFollowedByGarbage)
+{
   typedef typename TestFixture::CompressionInfo CompressionInfo;
 
   const int N = 10;
@@ -89,7 +136,7 @@ TYPED_TEST(DecodedDataStreamTest, compressedDataFollowedByGarbage) {
   const std::string compData = compress<CompressionInfo>(s*N);
   const std::string inputData = compData + std::string(10, '\0');
 
-  std::unique_ptr<zim::IDataStream> bds(new zim::BufDataStream(inputData.data(), inputData.size()));
+  std::unique_ptr<zim::IDataStream> bds(makeBufDataStream(&inputData));
   zim::DecodedDataStream<CompressionInfo> dds(std::move(bds), inputData.size());
   for (int i=0; i<N; i++)
   {
