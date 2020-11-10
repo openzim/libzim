82dfda68958bccfe69bc500de9b8479d4db6ea83
Matthieu Gautier <mgautier@kymeria.fr>
2020-11-10 23:43:30 +0100

    Add windows github CI

---
+++ .github/script/build_libzim.cmd
@@ -0,0 +1,10 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
+set CC=cl.exe
+set CXX=cl.exe
+
+meson.exe setup build . --force-fallback-for liblzma -Ddefault_library=static -Dzstd:bin_programs=false -Dzstd:bin_tests=false -Dzstd:bin_contrib=false -Dliblzma:default_library=static -Dliblzma:enable_xz=false
+
+cd build
+
+ninja.exe
