8db2816fcf3341e8fe1c6811d6c77e8d52c69759
Matthieu Gautier <mgautier@kymeria.fr>
2020-11-11 11:43:31 +0100

    Remove zlib support.
    We've stopped creating zim file with zlib compression a long time ago.
    Let's remove zlib dependency from libzim.
    
    Xapian depends on zlib.
    Passing `static-linkage=true` on ios makes meson also use the (xapian)
    private libraries at link. So we still link with the needed zlib
    (for xapian) even if libzim doesn't need it itself.

--- include/zim/zim.h
+++ include/zim/zim.h
@@ -53,7 +53,7 @@ namespace zim
   {
     zimcompDefault,
     zimcompNone,
-    zimcompZip,
+    zimcompZip, // Not supported anymore in the libzim
     zimcompBzip2, // Not supported anymore in the libzim
     zimcompLzma,
     zimcompZstd
