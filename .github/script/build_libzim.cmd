abf593eba122249fda7876bbdc6160139294ddbe
Matthieu Gautier <mgautier@kymeria.fr>
2020-12-15 17:24:54 +0100

    [CI] Build libzim without xapian too.
    Force compilation without xapian on windows.

--- .github/script/build_libzim.cmd
+++ .github/script/build_libzim.cmd
@@ -3,7 +3,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliar
 set CC=cl.exe
 set CXX=cl.exe
 
-meson.exe setup build . --force-fallback-for liblzma -Ddefault_library=static -Dzstd:bin_programs=false -Dzstd:bin_tests=false -Dzstd:bin_contrib=false -Dliblzma:default_library=static -Dliblzma:enable_xz=false
+meson.exe setup build . --force-fallback-for liblzma -Ddefault_library=static -Dwith_xapian=disabled -Dzstd:bin_programs=false -Dzstd:bin_tests=false -Dzstd:bin_contrib=false -Dliblzma:default_library=static -Dliblzma:enable_xz=false
 
 cd build
 
