80213a0465c9a1693350d409064e0e8d472d68dc
Matthieu Gautier <mgautier@kymeria.fr>
2021-01-12 17:16:44 +0100

    Correctly checkTitleIndex.
    We may now have two title index to check.

--- test/archive.cpp
+++ test/archive.cpp
@@ -233,12 +233,12 @@ TEST(ZimArchive, validate)
 
   EXPECT_BROKEN_ZIMFILE(
     "./data/invalid.outofbounds_first_title_entry.zim",
-    "Invalid title index entry\n"
+    "Invalid title index entry.\n"
   );
 
   EXPECT_BROKEN_ZIMFILE(
     "./data/invalid.outofbounds_last_title_entry.zim",
-    "Invalid title index entry\n"
+    "Invalid title index entry.\n"
   );
 
   EXPECT_BROKEN_ZIMFILE(
@@ -255,9 +255,7 @@ TEST(ZimArchive, validate)
 
   EXPECT_BROKEN_ZIMFILE(
     "./data/invalid.nonsorted_title_index.zim",
-    "Title index is not properly sorted:\n"
-    "  #0: A/Test ZIM file\n"
-    "  #1: -/favicon\n"
+    "Title index is not properly sorted.\n"
   );
 
   EXPECT_BROKEN_ZIMFILE(
