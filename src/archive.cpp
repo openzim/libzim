4a58bdbf5af244ac3efece62e38ddc885f8d9e60
Matthieu Gautier <mgautier@kymeria.fr>
2021-03-10 10:30:28 +0100

    Fix `iterByTitle`.
    If we have a specific title index, we must iterate on it.
    We must not iterate on a (wrong) subset of the entries.
    
    Related to #514

--- src/archive.cpp
+++ src/archive.cpp
@@ -239,7 +239,14 @@ namespace zim
 
   Archive::EntryRange<EntryOrder::titleOrder> Archive::iterByTitle() const
   {
-    return EntryRange<EntryOrder::titleOrder>(m_impl, m_impl->getStartUserEntry().v, m_impl->getEndUserEntry().v);
+    if (m_impl->hasFrontArticlesIndex()) {
+      // We have a front articles index. We can "simply" loop over all front entries.
+      return EntryRange<EntryOrder::titleOrder>(m_impl, 0, m_impl->getFrontEntryCount().v);
+    } else  {
+      // We don't have an index listing only front entry. We have to loop over user entry.
+      // (`C` namespace in new zim scheme, all namespace in old ones)
+      return EntryRange<EntryOrder::titleOrder>(m_impl, m_impl->getStartUserEntry().v, m_impl->getEndUserEntry().v);
+    }
   }
 
   Archive::EntryRange<EntryOrder::efficientOrder> Archive::iterEfficient() const
