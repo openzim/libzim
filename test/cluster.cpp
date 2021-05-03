467b148e79a982fd15fa34661adc1d5a16f48578
Veloman Yunkan <veloman.yunkan@gmail.com>
2021-05-04 00:15:23 +0400

    Demonstrating a bug in zim::writer::Cluster
    A `zim::writer::Cluster` doesn't detect the danger of obesity if it is
    being fed with 32-bit-sized items at a time. The modified
    `ClusterTest.read_write_extended_cluster` creates an extended cluster,
    which doesn't admit that it had overeaten:
    
    ```
    [ RUN      ] ClusterTest.read_write_extended_cluster
    ../../SOURCE/libzim/test/cluster.cpp:240: Failure
    Expected equality of these values:
      cluster.is_extended()
        Which is: false
      true
    [  FAILED  ] ClusterTest.read_write_extended_cluster (0 ms)
    ```

--- test/cluster.cpp
+++ test/cluster.cpp
@@ -226,8 +226,9 @@ TEST(ClusterTest, read_write_extended_cluster)
   std::string blob0("123456789012345678901234567890");
   std::string blob1("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
   std::string blob2("abcdefghijklmnopqrstuvwxyz");
-  zim::size_type bigger_than_4g = 1024LL*1024LL*1024LL*4LL+1024LL;
-  auto bigProvider = std::unique_ptr<zim::writer::ContentProvider>(new FakeProvider(bigger_than_4g));
+  const uint64_t FOUR_GIB = 4LL * 1024LL*1024LL*1024LL;
+  zim::size_type almost_4g = FOUR_GIB - 16;
+  auto bigProvider = std::unique_ptr<zim::writer::ContentProvider>(new FakeProvider(almost_4g));
 
   zim::writer::Cluster cluster(zim::zimcompNone);
   cluster.addContent(blob0);
@@ -235,6 +236,7 @@ TEST(ClusterTest, read_write_extended_cluster)
   cluster.addContent(blob2);
   cluster.addContent(std::move(bigProvider));
 
+  ASSERT_GT(cluster.size().v, FOUR_GIB);
   ASSERT_EQ(cluster.is_extended(), true);
 
   auto buffer = write_to_buffer(cluster);
@@ -248,7 +250,7 @@ TEST(ClusterTest, read_write_extended_cluster)
   ASSERT_EQ(cluster2.getBlobSize(zim::blob_index_t(0)).v, blob0.size());
   ASSERT_EQ(cluster2.getBlobSize(zim::blob_index_t(1)).v, blob1.size());
   ASSERT_EQ(cluster2.getBlobSize(zim::blob_index_t(2)).v, blob2.size());
-  ASSERT_EQ(cluster2.getBlobSize(zim::blob_index_t(3)).v, bigger_than_4g);
+  ASSERT_EQ(cluster2.getBlobSize(zim::blob_index_t(3)).v, almost_4g);
   ASSERT_EQ(blob0, std::string(cluster2.getBlob(zim::blob_index_t(0))));
   ASSERT_EQ(blob1, std::string(cluster2.getBlob(zim::blob_index_t(1))));
   ASSERT_EQ(blob2, std::string(cluster2.getBlob(zim::blob_index_t(2))));
