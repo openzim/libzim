8c76c8d4970e6e77bb4d8c7c66970ccdabc13086
Veloman Yunkan <veloman.yunkan@gmail.com>
2021-05-05 01:22:50 +0400

    Unit test improvement
    Improved the `ClusterTest.read_write_extended_cluster` unit-test
    by making sure that data at the end of the cluster is validated too.
    This would have caught the issue fixed in the previous commit.

--- test/cluster.cpp
+++ test/cluster.cpp
@@ -229,12 +229,14 @@ TEST(ClusterTest, read_write_extended_cluster)
   const uint64_t FOUR_GIB = 4LL * 1024LL*1024LL*1024LL;
   zim::size_type almost_4g = FOUR_GIB - 16;
   auto bigProvider = std::unique_ptr<zim::writer::ContentProvider>(new FakeProvider(almost_4g));
+  std::string blob4("zyxwvutsrqponmlkjihgfedcba");
 
   zim::writer::Cluster cluster(zim::zimcompNone);
   cluster.addContent(blob0);
   cluster.addContent(blob1);
   cluster.addContent(blob2);
   cluster.addContent(std::move(bigProvider));
+  cluster.addContent(blob4);
 
   ASSERT_GT(cluster.size().v, FOUR_GIB);
   ASSERT_EQ(cluster.is_extended(), true);
@@ -245,7 +247,7 @@ TEST(ClusterTest, read_write_extended_cluster)
   const auto cluster2shptr = zim::Cluster::read(zim::BufferReader(buffer), zim::offset_t(0));
   zim::Cluster& cluster2 = *cluster2shptr;
   ASSERT_EQ(cluster2.isExtended, true);
-  ASSERT_EQ(cluster2.count().v, 4U);
+  ASSERT_EQ(cluster2.count().v, 5U);
   ASSERT_EQ(cluster2.getCompression(), zim::zimcompNone);
   ASSERT_EQ(cluster2.getBlobSize(zim::blob_index_t(0)).v, blob0.size());
   ASSERT_EQ(cluster2.getBlobSize(zim::blob_index_t(1)).v, blob1.size());
@@ -254,6 +256,7 @@ TEST(ClusterTest, read_write_extended_cluster)
   ASSERT_EQ(blob0, std::string(cluster2.getBlob(zim::blob_index_t(0))));
   ASSERT_EQ(blob1, std::string(cluster2.getBlob(zim::blob_index_t(1))));
   ASSERT_EQ(blob2, std::string(cluster2.getBlob(zim::blob_index_t(2))));
+  ASSERT_EQ(blob4, std::string(cluster2.getBlob(zim::blob_index_t(4))));
 }
 
 
