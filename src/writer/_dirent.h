c97ac39a0b9d30a2fd43f5a7ac5ae7f837a00c5d
Matthieu Gautier <mgautier@kymeria.fr>
2021-08-26 12:00:52 +0200

    Move tinyString in its own file and add unittest.

--- src/writer/_dirent.h
+++ src/writer/_dirent.h
@@ -21,6 +21,7 @@
 #define ZIM_WRITER_DIRENT_H
 
 #include "cluster.h"
+#include "tinyString.h"
 
 #include "debug.h"
 
@@ -28,97 +29,6 @@ namespace zim
 {
   namespace writer {
     class Dirent;
-
-    struct TinyString {
-      TinyString() :
-        m_data(nullptr),
-        m_size(0)
-      {}
-      TinyString(const std::string& s) :
-        m_data(new char[(uint16_t)s.size()]),
-        m_size(s.size())
-      {
-        if (s.size() >= 0xFFFF) {
-          throw std::runtime_error("String len is too big");
-        }
-        std::memcpy(m_data, s.data(), m_size);
-      }
-      TinyString(TinyString&& t):
-        m_data(t.m_data),
-        m_size(t.m_size)
-      {
-        t.m_data = nullptr;
-        t.m_size = 0;
-      };
-      TinyString(const TinyString& t) = delete;
-      ~TinyString() {
-        if (m_data) {
-          delete[] m_data;
-          m_data = nullptr;
-        }
-      }
-      operator std::string() const { return std::string(m_data, m_size); }
-      bool empty() const { return m_size == 0; }
-      size_t size() const { return m_size; }
-      const char* const data() const { return m_data; }
-      bool operator==(const TinyString& other) const {
-        return (m_size == other.m_size) && (std::memcmp(m_data, other.m_data, m_size) == 0);
-      }
-      bool operator<(const TinyString& other) const {
-        auto min_size = std::min(m_size, other.m_size);
-        auto ret = std::memcmp(m_data, other.m_data, min_size);
-        if (ret == 0) {
-          return m_size < other.m_size;
-        } else {
-          return ret < 0;
-        }
-      }
-      char* m_data;
-      uint16_t m_size;
-    } PACKED;
-
-    struct PathTitleTinyString : TinyString {
-      PathTitleTinyString() : TinyString() {}
-      PathTitleTinyString(const std::string& path, const std::string& title)
-        : TinyString(PathTitleTinyString::concat(path, title))
-      {}
-
-      static std::string concat(const std::string& path, const std::string& title) {
-        std::string result(path.data(), path.size()+1);
-        if ( title != path ) {
-          result += title;
-        }
-        return result;
-      }
-      std::string getPath() const {
-        if (m_size == 0) {
-          return std::string();
-        }
-        return std::string(m_data);
-      }
-      std::string getTitle() const {
-        if (m_size == 0) {
-          return std::string();
-        }
-        auto title_start = std::strlen(m_data) + 1;
-        if (title_start == m_size) {
-          return std::string(m_data); // return the path as a title
-        } else {
-          return std::string(m_data+title_start, m_size-title_start);
-        }
-      }
-      std::string getStoredTitle() const {
-        if (m_size == 0) {
-          return std::string();
-        }
-        auto title_start = std::strlen(m_data) + 1;
-        if (title_start == m_size) {
-          return std::string(); // return empty title
-        } else {
-          return std::string(m_data+title_start, m_size-title_start);
-        }
-      }
-    } PACKED;
 
     struct DirentInfo {
       struct Direct {
