c71ea11860cdeb5262ab79ecad72e9eb81a071f0
Emmanuel Engelhart <kelson@kiwix.org>
2022-01-02 23:08:23 +0100

    Fix Emscripten compilation

--- src/debug.h
+++ src/debug.h
@@ -29,7 +29,7 @@
 # define ASSERT(left, operator, right) (void(0))
 #else
 
-#if !defined(_WIN32) && !defined(__APPLE__) && !defined(__ANDROID__)
+#if !defined(_WIN32) && !defined(__APPLE__) && !defined(__ANDROID__) && !defined(__EMSCRIPTEN__)
 #include <execinfo.h>
 #endif
 
@@ -41,7 +41,7 @@ void _on_assert_fail(const char* vara, const char* op, const char* varb,
       vara << "[" << a << "] " << op << " " << varb << "[" << b << "]";
   std::cerr << ss.str() << std::endl;
 
-#if !defined(_WIN32) && !defined(__APPLE__) && !defined(__ANDROID__) && defined(__GNU_LIBRARY__)
+#if !defined(_WIN32) && !defined(__APPLE__) && !defined(__ANDROID__) && !defined(__EMSCRIPTEN__) && defined(__GNU_LIBRARY__)
   void *callstack[64];
   size_t size;
   size = backtrace(callstack, 64);
