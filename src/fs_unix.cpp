a713f9d29a9be921998e36ea02e4d34959b64a83
Begasus <begasus@gmail.com>
2024-02-08 11:38:38 +0100

    Haiku build fix

--- src/fs_unix.cpp
+++ src/fs_unix.cpp
@@ -37,7 +37,7 @@ namespace unix {
 
 zsize_t FD::readAt(char* dest, zsize_t size, offset_t offset) const
 {
-#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__)
+#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__HAIKU__)
 # define PREAD pread
 #else
 # define PREAD pread64
